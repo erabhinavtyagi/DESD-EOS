2e94aa5a7289366e376047375373e7800afae5db
Abhinav Tyagi <erabhinavtyagi@gmail.com>
2022-05-24 09:36:46 +0530

    Changes done in Demo2 and Demo3.

--- Demo_Programs/02_piddemo.c
+++ Demo_Programs/02_piddemo.c
@@ -1,17 +1,18 @@
 // Process Id Demo
 
+#include<stdio.h>
 #include<sys/type.h>
 #include<unistd.h>
 
 int
 main ()
 {
-  int pid_t pid, pid_t ppid;
+  pid_t pid, pid_t ppid;
 
-  pid_tpid = getpid ();
-  pid_tppid = getppid ();
+  pid = getpid ();
+  ppid = getppid ();
 
-  printf ("Process ID : %d\n", pid_tpid);
-  printf ("Parent Process ID: %d\n", pid_tppid);
+  printf ("Process ID : %d\n", pid);
+  printf ("Parent Process ID: %d\n", ppid);
   return 0;
 }
--- Demo_Programs/03_libsyscalldemo.c
+++ Demo_Programs/03_libsyscalldemo.c
@@ -1,1 +1,15 @@
 //Library and System Call Demo
+
+#include <stdio.h>
+
+int main()
+{
+printf("Hello World\n");
+return 0;
+}
+
+
+// Run the executable file and check the STRACE. via command "strace ./filename"
+// there is one system call "write(1, "Hello World\n", 12Hello World)"
+
+
